1535ebbad926573b7fc046c9d92465694868f8d1
reguieg <reguiegyounes@gmail.com>
2022-04-25 13:59:09 +0100

    Create product entity

---
+++ API/Models/Product.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace API.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string  Name { get; set; }
+        public string Description { get; set; }
+        public long Price { get; set; }
+        public string PictureUrl { get; set; }
+        public string  Type { get; set; }
+        public string Brand { get; set; }
+        public int QuantityInStock { get; set; }
+    }
+}
