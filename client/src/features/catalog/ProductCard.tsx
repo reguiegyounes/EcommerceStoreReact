37946554bdbab36182cf88e8269f796aea109161
Younes Reguieg <reguiegyounes@gmail.com>
2022-07-25 13:50:02 +0100

    Styling the product card

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { ListItem, ListItemAvatar, Avatar, ListItemText } from "@mui/material"
+import { ListItem, ListItemAvatar, Avatar, ListItemText, Button, Card, CardActions, CardContent, CardMedia, Typography, CardHeader } from "@mui/material"
 import { Product } from "../../app/models/product";
 
 interface Props{
@@ -7,13 +7,35 @@ interface Props{
 
 export default function ProductCard({product}:Props){
     return (
-        <ListItem key={product.id}>
-            <ListItemAvatar>
-                <Avatar src={product.pictureUrl} />
-            </ListItemAvatar>
-            <ListItemText>
-                {product.name} - {product.price}
-            </ListItemText>
-        </ListItem>
+        <Card sx={{ maxWidth: 345 }}>
+            <CardHeader
+                avatar={
+                    <Avatar sx={{bgcolor:'secondary.main'}}>
+                        {product.name.charAt(0).toUpperCase()}
+                    </Avatar>
+                }
+                title={product.name}
+                titleTypographyProps={{
+                    sx : {fontWeight:'bold',color:'primary.main'}
+                }}
+            />
+            <CardMedia
+                sx={{height:140 , backgroundSize:'contain',bgcolor:'primary.light'}}
+                image={product.pictureUrl}
+                title={product.name}
+            />
+            <CardContent>
+                <Typography gutterBottom color='secondary' variant="h5">
+                    ${(product.price).toFixed(2)}
+                </Typography>
+                <Typography variant="body2" color="text.secondary"> 
+                    {product.brand} / {product.type}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Add to cart</Button>
+                <Button size="small">View</Button>
+            </CardActions>
+        </Card>
     )
 }
