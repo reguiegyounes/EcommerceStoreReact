0bbc72f8263c8abadb55568310a10c8e237fec23
Younes Reguieg <reguiegyounes@gmail.com>
2022-07-25 23:32:54 +0100

    Refactoring in catalog component

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,34 +1,10 @@
 import { Container, CssBaseline } from "@mui/material";
-import { useEffect, useState } from "react";
 import Catalog from "../../features/catalog/Catalog";
-import { Product } from "../models/product";
 import Header from "./Header";
 
 
 function App() {
-  const [products,setProducts]=useState<Product[] >([]);
-
-  useEffect(()=>{
-    
-    fetch("http://localhost:5000/api/products")
-    .then(response => response.json())
-    .then(data => setProducts(data))
-
-  },[])
-
-  function addProduct() {
-    setProducts([
-      ...products,
-      {
-        id:products.length+101,
-        name:"product"+(products.length+1),
-        price:(products.length+1)*100,
-        brand:'some brand',
-        description: 'some description',
-        pictureUrl:'http://picsum.photos/200'
-      }
-    ]);
-  }
+  
 
 
   return (
@@ -37,7 +13,7 @@ function App() {
       <CssBaseline/>
       <Header/>
       <Container>
-        <Catalog products={products} addProduct={addProduct}/>
+        <Catalog/>
       </Container>
       
     </>
--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,20 +1,20 @@
-import { Button } from "@mui/material";
+import { useState, useEffect } from "react";
 import { Product } from "../../app/models/product";
 import ProductsList from "./ProductsList";
 
+export default function Catalog(){
+    const [products,setProducts]=useState<Product[] >([]);
 
+    useEffect(()=>{
+        fetch("http://localhost:5000/api/products")
+        .then(response => response.json())
+        .then(data => setProducts(data))
 
-interface Props{
-    products:Product[];
-    addProduct:()=>void;
-}
+    },[])
 
-
-export default function Catalog({products,addProduct}:Props){
     return (
-        <>
-            <Button variant="contained" onClick={addProduct}>Add Product</Button>
-            <ProductsList products={products}/>
-        </>
+    
+        <ProductsList products={products}/>
+      
     );
 }
