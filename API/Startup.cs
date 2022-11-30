5714ad400f51c74c9a64ecbc16757d93b98bbc92
Younes Reguieg <reguiegyounes@gmail.com>
2022-11-30 09:22:46 +0100

    Add Credentials to Cors

--- API/Startup.cs
+++ API/Startup.cs
@@ -54,6 +54,7 @@ namespace API
             app.UseCors(opt=>{
                 opt.AllowAnyHeader()
                 .AllowAnyMethod()
+                .AllowCredentials()
                 .WithOrigins("http://localhost:3000");
             });
             app.UseAuthorization();
