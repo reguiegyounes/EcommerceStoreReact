c0250aad8ff25490a322c6405320269d376500f8
Younes Reguieg <reguiegyounes@gmail.com>
2022-12-04 19:24:01 +0100

    Styling the basket page with a table

--- client/src/features/basket/BasketPage.tsx
+++ client/src/features/basket/BasketPage.tsx
@@ -1,4 +1,5 @@
-import { Typography } from "@mui/material";
+import { Delete } from "@mui/icons-material";
+import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import { useEffect, useState } from "react"
 import agent from "../../app/api/agent";
 import LoadingComponent from "../../app/layout/LoadingComponent";
@@ -6,8 +7,8 @@ import { Basket } from "../../app/models/basket";
 
 
 export default function BasketPage() {
-    const [loading,setLoading] = useState(true);
-    const [basket,setBasket] = useState<Basket | null>(null);
+    const [loading, setLoading] = useState(true);
+    const [basket, setBasket] = useState<Basket | null>(null);
 
 
     const getbasket = async () => {
@@ -24,13 +25,44 @@ export default function BasketPage() {
 
     useEffect(() => {
         getbasket();
-    },[]);
-    
+    }, []);
 
-    if (loading) return <LoadingComponent message='Loading Basket...'/>
+
+    if (loading) return <LoadingComponent message='Loading Basket...' />
 
     if (!basket) return <Typography variant="h3">Your basket is empty</Typography>
     return (
-        <h1>{basket.buyerId}</h1>
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Product</TableCell>
+                        <TableCell align="right">Price</TableCell>
+                        <TableCell align="right">Quantity</TableCell>
+                        <TableCell align="right">SubTotal</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {basket.items.map((item) => (
+                        <TableRow
+                            key={item.productId}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}  
+                        >
+                            <TableCell component="th" scope="row">
+                                {item.name}
+                            </TableCell>
+                            <TableCell align="right">{item.price.toFixed(2)}</TableCell>
+                            <TableCell align="right">{item.quantity}</TableCell>
+                            <TableCell align="right">{(item.price * item.quantity).toFixed(2)}</TableCell>
+                            <TableCell align="right">
+                                <IconButton color='error'>
+                                    <Delete />
+                                </IconButton>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     )
 }
