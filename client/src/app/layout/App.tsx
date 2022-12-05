bb05ee0b01f81642a35e36a51d526db32fdbd828
Younes Reguieg <reguiegyounes@gmail.com>
2022-12-05 09:58:49 +0100

    App initialisation - fetching the basket on app start

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,5 +1,10 @@
-import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
-import { useState } from "react";
+import {
+  Container,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+} from "@mui/material";
+import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import AboutPage from "../../features/about/AboutPage";
@@ -8,48 +13,69 @@ import ProductDetails from "../../features/catalog/ProductDetails";
 import ContactPage from "../../features/contact/ContactPage";
 import HomePage from "../../features/home/HomePage";
 import Header from "./Header";
-import 'react-toastify/dist/ReactToastify.css'
+import "react-toastify/dist/ReactToastify.css";
 import ServerError from "../errors/ServerError";
 import NotFound from "../errors/NotFound";
 import BasketPage from "../../features/basket/BasketPage";
-
+import { useStoreContext } from "../context/StoreContext";
+import agent from "../api/agent";
+import LoadingComponent from "./LoadingComponent";
 
 function App() {
-  const [darkMode,setDarkMode]=useState(false)
-  const paletteType = darkMode? 'dark':'light'
-  const theme =createTheme({
-    palette:{
-      mode:paletteType ,
-      background:{
-        default: paletteType === 'light'? '#eaeaea':'#121212'
-      }
-    }
-  })
+  const { setBasket } = useStoreContext();
+  const [loading, setLoading] = useState(true);
 
-  function handleThemeChange(){
-    setDarkMode(!darkMode)
+  const getbasket = async () => {
+    setLoading(true);
+    try {
+      const basket = await agent.BasketApi.get();
+      setBasket(basket);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getbasket();
+  }, [setBasket]);
+
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? "dark" : "light";
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === "light" ? "#eaeaea" : "#121212",
+      },
+    },
+  });
+
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
   }
 
+  if (loading) return <LoadingComponent message="Initialising app..." /> 
+  
   return (
-
     <ThemeProvider theme={theme}>
-      <ToastContainer position='bottom-right' hideProgressBar  theme="colored"/>
-      <CssBaseline/>
-      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
+      <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
         <Routes>
-          <Route path='/' element={<HomePage/>}/>
-          <Route path="/catalog" element={<Catalog/>}/>
-          <Route path="/catalog/:id" element={<ProductDetails/>}/>
-          <Route path="/contact" element={<ContactPage/>}/>
-          <Route path="/about" element={<AboutPage/>}/>
-          <Route path="/server-error" element={<ServerError/>}/>
-          <Route path="/basket" element={<BasketPage/>}/>
-          <Route path="/*" element={<NotFound/>}/>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/catalog/:id" element={<ProductDetails />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/server-error" element={<ServerError />} />
+          <Route path="/basket" element={<BasketPage />} />
+          <Route path="/*" element={<NotFound />} />
         </Routes>
       </Container>
     </ThemeProvider>
-
   );
 }
 
