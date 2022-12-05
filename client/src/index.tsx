7e05ddec56a924cef3fda74bf26d5ae93c8bf9c5
Younes Reguieg <reguiegyounes@gmail.com>
2022-12-05 09:03:22 +0100

    Using React context to centralise state

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,6 +5,7 @@ import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { unstable_HistoryRouter as HistoryRouter } from "react-router-dom";
 import {createBrowserHistory} from 'history';
+import { StoreProvider } from './app/context/StoreContext';
 
 export const history=createBrowserHistory();
 
@@ -14,7 +15,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <HistoryRouter  history={history}>
-      <App />
+      <StoreProvider>
+        <App />
+      </StoreProvider>
     </HistoryRouter>
   </React.StrictMode>
 );
